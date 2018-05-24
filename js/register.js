cf37e5ebd6bb97cefd8070d1356d2db951edaef4
hhttrang <hatasatori514@gmail.com>
2018-05-25 05:42:30 +0700

    add front-end login & register

--- js/register.js
+++ js/register.js
@@ -65,8 +65,11 @@ function checkregister(){
 		if(agree.checked==false){
 			flag=false;
 			window.alert("Bạn phải đồng ý với điều khoản clb");
-		}  
-		return flag;
+		}
+		if(flag == true){
+			var notiBox = document.getElementById("noti_field");
+			notiBox.style.display="block";  
+		}   
 } 
  function openTerm(){  
 	var content1 = document.getElementById("term_content");
@@ -87,4 +90,50 @@ function animation_deco(){
 }
 function run_deco(){
 		window.setInterval(animation_deco,3000);
+}
+function confirmNoti(flag){
+	if(flag == true){
+		var notiBox = document.getElementById("noti_field");
+	notiBox.style.display="block"; 
 	}
+	 
+} 
+function exitNoti(){
+	var notiBox=document.getElementById("noti_field");
+	notiBox.style.display="none";
+	window.open("index.html",'_self'); 
+} 
+
+function checkUserLogin(){ 
+		var loginUsername=document.getElementById("loginUsername"); 
+		var loginPassword=document.getElementById("loginPassword"); 
+
+		var alertlogin1=document.getElementById("alertlogin1");
+		var alertlogin2=document.getElementById("alertlogin2");  
+		var mau1=/^([a-zA-Z0-9]{6,})$/;        
+		var mau2=/^([a-zA-Z0-9\s]{6,})$/; 
+		var flag=true;
+
+		if(mau1.test(loginUsername.value)==false){  
+			flag=false; 
+			loginUsername.style.borderColor = "red";       
+			alertlogin1.style.visibility="visible";
+		} 
+ 		else {
+ 			loginUsername.style.borderColor = "lightblue";
+ 			alertlogin1.style.visibility="hidden"; 
+ 		}
+ 
+		if(mau2.test(loginPassword.value)==false){
+			flag=false; 
+			loginPassword.style.borderColor = "red";
+			alertlogin2.style.visibility="visible";
+		} 
+		else{
+			loginPassword.style.borderColor = "lightblue";  
+			alertlogin2.style.visibility="hidden"; 
+		} 
+		if(flag == true){
+			window.open("index.html",'_self'); 
+		}   
+}
