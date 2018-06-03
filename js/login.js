96487773e3e547e9f41ac73d1f5a720045ead596
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2018-06-03 19:49:24 +0700

    add edit field(profile.html)

--- js/login.js
+++ js/login.js
@@ -6,4 +6,39 @@ function showLogin()
     } else{
                 login.style.display = "none";
     }
+
+    
 }
+$(document).ready(function(){
+
+var edit_btn = $(".edit-btn");
+
+  edit_btn.each(function(){
+
+
+    this.addEventListener("click",function(){
+      var edit_field = $(this).parent().next().find(".edit-field");
+    var wrapper = $(this).parent().next();
+    console.log(edit_field);
+    console.log(wrapper);
+     if(edit_field.attr('data-display')=="true"){
+      wrapper.animate({
+        left: "-100%"
+      },500,function(){
+        edit_field.attr("data-display","false");
+      });
+      
+    }
+    else {
+       wrapper.animate({
+        left: "0"
+      },500,function(){
+        edit_field.attr("data-display","true");
+      });
+      
+    }
+
+  });
+  });
+
+});
